149ba2d8dff85db5ea3ccc8a0e82e3b9bb53b82a
ignaciotitos <titosignacio8@gmail.com>
2022-06-01 18:02:07 +0200

    añadido de urrlib para quitar warnings

--- python/scrapper_filmAffinity.py
+++ python/scrapper_filmAffinity.py
@@ -1,5 +1,6 @@
 from bs4 import BeautifulSoup
 import requests
+import urllib3
 import python_filmaffinity
 #website = 'https://www.filmaffinity.com/es/main.html'
 #response = requests.get(website)
@@ -11,6 +12,7 @@ import python_filmaffinity
 
 #<div id="movie-rat-avg" itemprop="ratingValue" content="7.8"> 7,8 </div>
 
+urllib3.disable_warnings()
 service = python_filmaffinity.FilmAffinity()
 movie = service.get_movie(title='Indiana Jones 5')
 print(movie['title'])
